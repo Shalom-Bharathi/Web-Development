489337661e586028e531faf0d1938db35999d313
Shalom Bharathi <154990142+Shalom-Bharathi@users.noreply.github.com>
2024-02-11 22:52:48 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,13 @@
+const oberserver = new IntersectionObserver ((entries) => {
+    entries.forEach((entry) => {
+        console.log(entry)
+        if (entry.isIntersecting) {
+            entry.target.classList.add('show');
+        } else {
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements =  document.querySelectorAll('.hidden');
+hiddenElements.forEach((el) => oberserver.observe(el));
